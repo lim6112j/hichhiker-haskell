04f46850bd08755f82bd779f1117a26da94d3f26
lim6112j <lim6112j@gmail.com>
2022-06-03 04:06:56 +0900

    skeleton of cd-fit

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
     ( someFunc
     ) where
-
+c = putStrLn "C!"
+combine before after =
+  do before
+     putStrLn "In the middle"
+     after
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  combine c c
+  let b = combine (putStrLn "Hello!") (putStrLn "Bye!")
+  let d = combine (b) (combine c c)
+  putStrLn "So Long!"
